ba2234b37cc0054d5528ac227ef6ef924a3053db
Sunjin Lee <styner32@gmail.com>
2016-05-10 04:26:21 +0000

    redirect http to https if `force_https` is true

--- prefix_main.lua
+++ prefix_main.lua
@@ -1,6 +1,7 @@
 local prefix_handler = require('prefix_handler')
+local config = require('config')
 
-local prefix, err, err_log = prefix_handler.handle(ngx.var.host, ngx.var.request_uri)
+local prefix, force_https, err, err_log = prefix_handler.handle(ngx.var.host, ngx.var.request_uri)
 
 if err then
   if err_log then
@@ -18,6 +19,16 @@ if err then
   end
 
   return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+end
+
+if force_https then
+  local https_url
+  if config.ssl_port then
+    https_url = "https://"..ngx.var.host..":"..config.ssl_port..ngx.var.request_uri
+  else
+    https_url = "https://"..ngx.var.host..ngx.var.request_uri
+  end
+  return ngx.redirect(https_url, ngx.HTTP_MOVED_TEMPORARILY)
 end
 
 ngx.var.rise_prefix = prefix
